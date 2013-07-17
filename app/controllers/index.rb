09388befd96f13d2d89bc8d60b695db026c3cddd
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 16:43:55 -0500

    working on ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,6 +7,7 @@ end
 post '/rolls' do
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
+  p "Params = #{params[:value]}"
   value = params[:value] ? params[:value].to_i : nil
 
   @roll = value ? Roll.create({ value: value }) : Roll.create
--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,30 @@
 $(document).ready(function () {
-
-  // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
+  $('#button_form').on('submit', '#button_form', function(event){
+
   //   2- prevent the default action for that event from happening
+      event.preventDefault();
+
   //   3- generate a random number between 1 and 6 using JavaScript
+      var random_number = Math.floor(Math.random() * 6) + 1;
+   
   //   4- use jQuery to submit an AJAX post to the form's action
+       var url  = $(this).attr('action');
+       $.post(url, function(event2){
+          
+       })
+
+
+
+    });
+  });
+});
+
+
+
+// PSEUDO-CODE:
+
+
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
-});
+
